7ec875d6102d1e6b8096e3c212ad1b8b1a131de0
Wanve Abhishek Ram <wanveabhishekram@192.168.1.215>
2026-06-27 12:02:52 +0530

    feat: point app at production backend (flasky-qn0j.onrender.com)

--- rn_app/src/core/api.js
+++ rn_app/src/core/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { DeviceEventEmitter } from 'react-native';
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://10.0.2.2:8080';
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL ?? 'https://flasky-qn0j.onrender.com';
 
 export const http = axios.create({ baseURL: BASE_URL });
 
